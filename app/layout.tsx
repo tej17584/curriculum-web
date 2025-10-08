33b1a1c4dcf4b1ef9e861ec532a25e9310c5f98a
tejon17584 <alejandro.tejada.ui@gmail.com>
2025-10-08 11:43:27 -0600

    Updating book animation on dark mode

--- app/layout.tsx
+++ app/layout.tsx
@@ -27,6 +27,21 @@ export default async function RootLayout({
       lang={lang}
       suppressHydrationWarning
     >
+      <head>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              try {
+                if (localStorage.theme === 'dark' || (!('theme' in localStorage) && window.matchMedia('(prefers-color-scheme: dark)').matches)) {
+                  document.documentElement.classList.add('dark')
+                } else {
+                  document.documentElement.classList.remove('dark')
+                }
+              } catch (_) {}
+            `,
+          }}
+        />
+      </head>
       <body
         className={`font-sans ${GeistSans.variable} ${GeistMono.variable} ${neuton.variable}`}
       >
